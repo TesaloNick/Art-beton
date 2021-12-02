da7b573ffdce18e543e5ae0551b76688e58e4904
Nikolay Antonovich <TesaloNick@gmail.com>
2021-12-02 22:20:43 +0300

    art-beton3

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@ $('.slider-block__slider').slick({
   customPaging: function(slider, i) {
     return '<div class="slider-block__dot"></div>';
   },
-  prevArrow: '<button id="prev" type="button" class="slider-block__arrow-left"><img src="./img/slider/arrow-left.svg" alt="arrow-left" class="slider-block__arrow-left-img"></button>',
-  nextArrow: '<button id="next" type="button" class="slider-block__arrow-right"><img src="./img/slider/arrow-right.svg" alt="arrow-right" class="slider-block__arrow-right-img"></button>',
+  prevArrow: '<button type="button" class="slider-block__arrow-left"><img src="../img/slider/arrow-left.svg" alt="arrow-left" class="slider-block__arrow-left-img"></button>',
+  nextArrow: '<button type="button" class="slider-block__arrow-right"><img src="../img/slider/arrow-right.svg" alt="arrow-right" class="slider-block__arrow-right-img"></button>',
 });
 
 if ($(window).width() <= 1150) {
   $('.slider-of-prices__block').slick({
     dots: false,
     focusOnSelect: true,
-    prevArrow: '<button id="prev" type="button" class="slider-block__arrow-left"><img src="./img/slider/arrow-left.svg" alt="arrow-left" class="slider-block__arrow-left-img"></button>',
-    nextArrow: '<button id="next" type="button" class="slider-block__arrow-right"><img src="./img/slider/arrow-right.svg" alt="arrow-right" class="slider-block__arrow-right-img"></button>',
+    prevArrow: '<button type="button" class="slider-block__arrow-left"><img src="./img/slider/arrow-left.svg" alt="arrow-left" class="slider-block__arrow-left-img"></button>',
+    nextArrow: '<button type="button" class="slider-block__arrow-right"><img src="./img/slider/arrow-right.svg" alt="arrow-right" class="slider-block__arrow-right-img"></button>',
     slidesToShow: 3,
     centerPadding: '60px',
     responsive: [
@@ -85,27 +85,43 @@ const getHelpButton = document.querySelector('.get-help__button')
 const getHelpBlockBackground = document.querySelector('.get-help__block-background')
 const getHelpWindow = document.querySelector('.get-help__window')
 const getHelpSendButton = document.querySelector('.get-help__send-button')
+const getHelpCloseForm = document.querySelector('.get-help__close-form')
 
+function openGetHelpWindow() {
+  getHelpBlockBackground.style.backgroundColor = 'rgba(204, 204, 204, 0.376)'
+  getHelpButton.style.backgroundImage = 'url(../img/get-help/icon-cross.png)'
+  getHelpButton.style.backgroundColor = '#CCCCCC'
+  getHelpWindow.style.opacity = '1'  
+  getHelpWindow.style.visibility = 'visible'  
+  getHelpWindow.style.transform = 'translate(0px, 0px)'  
+  getHelpBlockBackground.style.opacity = '1'  
+  getHelpBlockBackground.style.visibility = 'visible'  
+}
+
+function closeGetHelpWindow() {
+  getHelpBlockBackground.style.backgroundColor = 'rgba(204, 204, 204, 0)'
+  getHelpButton.style.backgroundImage = 'url(../img/get-help/icon-speech.png)'
+  getHelpButton.style.backgroundColor = '#FFE600'
+  getHelpWindow.style.opacity = '0'  
+  getHelpWindow.style.visibility = 'hidden'  
+  getHelpWindow.style.transform = 'translate(100%, 0px)'  
+  getHelpBlockBackground.style.opacity = '0'  
+  getHelpBlockBackground.style.visibility = 'hidden'  
+}
 
 getHelpButton.addEventListener('click', () => {
-  console.log(getHelpBlockBackground.style.backgroundColor);
   if (getHelpBlockBackground.style.backgroundColor == 'rgba(204, 204, 204, 0.376)') {
-    getHelpBlockBackground.style.backgroundColor = 'rgba(204, 204, 204, 0)'
-    getHelpButton.style.backgroundImage = 'url(./img/get-help/icon-speech.png)'
-    getHelpButton.style.backgroundColor = '#FFE600'
-    getHelpWindow.style.display = 'none'
-    getHelpBlockBackground.style.display = 'none'
+    closeGetHelpWindow()
   } else{
-    getHelpBlockBackground.style.backgroundColor = 'rgba(204, 204, 204, 0.376)'
-    getHelpButton.style.backgroundImage = 'url(./img/get-help/icon-cross.png)'
-    getHelpButton.style.backgroundColor = '#CCCCCC'
-    getHelpWindow.style.display = 'grid'  
-    getHelpBlockBackground.style.display = 'block'  
+    openGetHelpWindow()
   }
 })
-getHelpSendButton.addEventListener('click', () => {
-  getHelpBlockBackground.style.backgroundColor = 'rgba(204, 204, 204, 0)'
-  getHelpButton.style.backgroundImage = 'url(./img/get-help/icon-speech.png)'
-  getHelpButton.style.backgroundColor = '#FFE600'
-  getHelpWindow.style.display = 'none'
+
+getHelpSendButton.addEventListener('click', (event) => {
+  event.preventDefault()
+  closeGetHelpWindow()
+})
+
+getHelpCloseForm.addEventListener('click', () => {
+  closeGetHelpWindow()
 })
